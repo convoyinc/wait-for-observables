36a6277ca78ca4a761700a004b514e4964e15829
Ian MacLeod <ian@nevir.net>
2017-12-04 15:43:03 -0800

    Bump a PR to force a circle workflow build

--- src/types.ts
+++ src/types.ts
@@ -1,6 +1,4 @@
 // TODO: Remove once this makes its way into standard typings.
-//
-// We're only typing the portions of the Observable interface that we use.
 export interface Observable {
   subscribe(observer: Observer<any>): Subscription;
 }
